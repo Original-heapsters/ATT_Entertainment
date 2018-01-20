19dcc2d1af7d9aeddea68aa1546cb03585be94bc
Russell <sell_nat@yahoo.com>
2018-01-20 02:22:51 -0800

    Removed all the junk methods

--- app.py
+++ app.py
@@ -17,27 +17,6 @@ def index():
     indexObj = {"one":"two"}
     return json.dumps(indexObj)
 
-@app.route('/upload')
-def upload():
-    sample = {'ObjectInterpolator': 1629,  'PointInterpolator': 1675, 'RectangleInterpolator': 2042}
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return "Missing file"
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            return "Filename is blank"
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "File upload success"
-    return
-
 @app.route('/uploadalbum', methods=['POST'])
 def upload_album():
     fileUrls = []
@@ -53,17 +32,16 @@ def upload_album():
                 shutil.rmtree(file_path)
         except Exception as e:
             print(e)
-    if request.method == 'POST':
         # check if the post request has the file part
         if 'files[]' not in request.files:
-            return "Missing file"
+            return json.dumps({"ERROR":"Missing files[] object"}, indent=4)
         fileList = request.files.getlist('files[]')
         print(fileList)
         # if user does not select file, browser also
         # submit a empty part without filename
         for file in fileList:
             if file.filename == '':
-                return "Filename is blank"
+                return json.dumps({"ERROR":"filename is blank"}, indent=4)
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 directory = app.config['UPLOAD_FOLDER'] + '/' + albumId
@@ -72,10 +50,7 @@ def upload_album():
                 filePath = os.path.join(directory, filename)
                 fileUrls.append(request.url_root + filePath)
                 file.save(filePath)
-        return json.dumps(fileUrls, indent=4)
-    return
-
-    return json.dumps(sample, indent=4, sort_keys=True)
+        return json.dumps({"uploaded_photos":fileUrls}, indent=4)
 
 @app.route('/analyze')
 def analyze():
