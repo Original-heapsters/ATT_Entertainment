9e9ec146627f77b284287943aee90622537eb263
Russell <sell_nat@yahoo.com>
2018-01-20 02:54:49 -0800

    Indentation oopsie

--- app.py
+++ app.py
@@ -33,26 +33,26 @@ def upload_album():
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print(e)
-        # check if the post request has the file part
-        if 'files[]' not in request.files:
-            return json.dumps({"ERROR":"Missing files[] object"}, indent=4)
-        fileList = request.files.getlist('files[]')
-        print(fileList)
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        for file in fileList:
-            if file.filename == '':
-                return json.dumps({"ERROR":"filename is blank"}, indent=4)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                directory = app.config['UPLOAD_FOLDER'] + '/' + albumId
-                if not os.path.exists(directory):
-                    os.makedirs(directory)
-                filePath = os.path.join(directory, filename)
-                fileUrls.append(request.url_root + filePath)
-                file.save(filePath)
-        return json.dumps({"uploaded_photos":fileUrls}, indent=4)
+            return json.dumps({"ERROR":e})
+    # check if the post request has the file part
+    if 'files[]' not in request.files:
+        return json.dumps({"ERROR":"Missing files[] object"}, indent=4)
+    fileList = request.files.getlist('files[]')
+    print(fileList)
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    for file in fileList:
+        if file.filename == '':
+            return json.dumps({"ERROR":"filename is blank"}, indent=4)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            directory = app.config['UPLOAD_FOLDER'] + '/' + albumId
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            filePath = os.path.join(directory, filename)
+            fileUrls.append(request.url_root + filePath)
+            file.save(filePath)
+    return json.dumps({"uploaded_photos":fileUrls}, indent=4)
 
 @app.route('/analyze')
 def analyze():
